025bbe5242504fccce9d5a3d8f9aa06dae44c63c
hex <72384846+H3x-i@users.noreply.github.com>
2023-05-17 16:06:08 +0100

    main.c

--- ESP/main/main.c
+++ ESP/main/main.c
@@ -21,10 +21,84 @@
 
 #define LED_PIN 2
 
-char on_resp[] = "<!DOCTYPE html><html><head><style type=\"text/css\">html {  font-family: Arial;  display: inline-block;  margin: 0px auto;  text-align: center;}h1{  color: #070812;  padding: 2vh;}.button {  display: inline-block;  background-color: #b30000; //red color  border: none;  border-radius: 4px;  color: white;  padding: 16px 40px;  text-decoration: none;  font-size: 30px;  margin: 2px;  cursor: pointer;}.button2 {  background-color: #364cf4; //blue color}.content {   padding: 50px;}.card-grid {  max-width: 800px;  margin: 0 auto;  display: grid;  grid-gap: 2rem;  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));}.card {  background-color: white;  box-shadow: 2px 2px 12px 1px rgba(140,140,140,.5);}.card-title {  font-size: 1.2rem;  font-weight: bold;  color: #034078}</style>  <title>ESP32 WEB SERVER</title>  <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">  <link rel=\"icon\" href=\"data:,\">  <link rel=\"stylesheet\" href=\"https://use.fontawesome.com/releases/v5.7.2/css/all.css\"    integrity=\"sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr\" crossorigin=\"anonymous\">  <link rel=\"stylesheet\" type=\"text/css\" ></head><body>  <h2>ESP32 WEB SERVER</h2>  <div class=\"content\">    <div class=\"card-grid\">      <div class=\"card\">        <p><i class=\"fas fa-lightbulb fa-2x\" style=\"color:#c81919;\"></i>     <strong>GPIO2</strong></p>        <p>GPIO state: <strong> ON</strong></p>        <p>          <a href=\"/led2on\"><button class=\"button\">ON</button></a>          <a href=\"/led2off\"><button class=\"button button2\">OFF</button></a>        </p>      </div>    </div>  </div></body></html>";
+char on_resp[] = "<!DOCTYPE html><html>
+    <head><style type=\"text/css\">html {  font-family: Arial;  display: inline-block;  margin: 0px auto;  text-align: center;}h1{  color: #070812;  padding: 2vh;}.button {  display: inline-block;  background-color: #b30000; //red color  border: none;  border-radius: 4px;  color: white;  padding: 16px 40px;  text-decoration: none;  font-size: 30px;  margin: 2px;  cursor: pointer;}.button2 {  background-color: #364cf4; //blue color}.content {   padding: 50px;}.card-grid {  max-width: 800px;  margin: 0 auto;  display: grid;  grid-gap: 2rem;  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));}.card {  background-color: white;  box-shadow: 2px 2px 12px 1px rgba(140,140,140,.5);}.card-title {  font-size: 1.2rem;  font-weight: bold;  color: #034078}</style>
+      <title>ESP32 WEB SERVER</title>  
+      <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\"> 
+      <link rel=\"icon\" href=\"data:,\">  <link rel=\"stylesheet\" href=\"https://use.fontawesome.com/releases/v5.7.2/css/all.css\"    integrity=\"sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr\" crossorigin=\"anonymous\">  <link rel=\"stylesheet\" type=\"text/css\" ></head>
+      <body>  <h2>ESP32 WEB SERVER</h2>  
+              <div class=\"content\">    
+              <div class=\"card-grid\">    
+              <div class=\"card\">       
+              <p><i class=\"fas fa-lightbulb fa-2x\" style=\"color:#c81919;\"></i>     <strong>HOUSE COMMAND</strong></p>        
+              <p>hall state: <strong> ON</strong></p>        
+              <p><a href=\"/led2on\"><button class=\"button\">ON</button></a> 
+              <a href=\"/led2off\"><button class=\"button button1\">OFF</button></a></p></div></div></div>
+              
+              <p>cuisine state: <strong> ON</strong></p> 
+              <p><a href=\"/led1on\"><button class=\"button\">ON</button></a> 
+              <a href=\"/led1off\"><button class=\"button button2\">OFF</button></a></p></div></div></div>
+              
+              <p>salon state: <strong> ON</strong></p>  
+              <p><a href=\"/led3on\"><button class=\"button\">ON</button></a> 
+              <a href=\"/led3off\"><button class=\"button button3\">OFF</button></a></p></div></div></div>
+              
+              <p>VC state: <strong> ON</strong></p>  
+              <p><a href=\"/led4on\"><button class=\"button\">ON</button></a> 
+              <a href=\"/led4off\"><button class=\"button button4\">OFF</button></a></p></div></div></div>
+              
+              <p>C.A state: <strong> ON</strong></p>  
+              <p><a href=\"/led5on\"><button class=\"button\">ON</button></a> 
+              <a href=\"/led5off\"><button class=\"button button5\">OFF</button></a></p></div></div></div>
+              
+              <p>C.P state: <strong> ON</strong></p>  
+              <p><a href=\"/led6on\"><button class=\"button\">ON</button></a> 
+              <a href=\"/led6off\"><button class=\"button button6\">OFF</button></a></p></div></div></div>
+              
+              <p>C.C state: <strong> ON</strong></p>  
+              <p><a href=\"/led7on\"><button class=\"button\">ON</button></a> 
+              <a href=\"/led7off\"><button class=\"button button7\">OFF</button></a></p></div></div></div>
+                  
+      
+      
+      </body></html>";
+  
+char off_resp[] = "<!DOCTYPE html><html>
+    <head><style type=\"text/css\">html {  font-family: Arial;  display: inline-block;  margin: 0px auto;  text-align: center;}h1{  color: #070812;  padding: 2vh;}.button {  display: inline-block;  background-color: #b30000; //red color  border: none;  border-radius: 4px;  color: white;  padding: 16px 40px;  text-decoration: none;  font-size: 30px;  margin: 2px;  cursor: pointer;}.button2 {  background-color: #364cf4; //blue color}.content {   padding: 50px;}.card-grid {  max-width: 800px;  margin: 0 auto;  display: grid;  grid-gap: 2rem;  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));}.card {  background-color: white;  box-shadow: 2px 2px 12px 1px rgba(140,140,140,.5);}.card-title {  font-size: 1.2rem;  font-weight: bold;  color: #034078}</style>
+      <title>ESP32 WEB SERVER</title>  
+      <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\"> 
+      <link rel=\"icon\" href=\"data:,\">  <link rel=\"stylesheet\" href=\"https://use.fontawesome.com/releases/v5.7.2/css/all.css\"    integrity=\"sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr\" crossorigin=\"anonymous\">  <link rel=\"stylesheet\" type=\"text/css\" ></head>
+      <body>  <h2>ESP32 WEB SERVER</h2>  
+              <div class=\"content\">    
+              <div class=\"card-grid\">    
+              <div class=\"card\">       
+              <p><i class=\"fas fa-lightbulb fa-2x\" style=\"color:#c81919;\"></i>     <strong>HOUSE COMMAND</strong></p>        
+              <p>hall state: <strong> OFF</strong></p>        
+              <p><a href=\"/led2on\"><button class=\"button\">ON</button></a> 
+              <a href=\"/led2off\"><button class=\"button button1\">OFF</button></a></p></div></div></div>
+              <p>cuisine state: <strong> OFF</strong></p>  
+              <p><a href=\"/led1on\"><button class=\"button\">ON</button></a> 
+              <a href=\"/led1off\"><button class=\"button button2\">OFF</button></a></p></div></div></div>
+              <p>salon state: <strong> OFF</strong></p>  
+              <p><a href=\"/led3on\"><button class=\"button\">ON</button></a> 
+              <a href=\"/led3off\"><button class=\"button button3\">OFF</button></a></p></div></div></div>
+              <p>VC state: <strong> OFF</strong></p>  
+              <p><a href=\"/led4on\"><button class=\"button\">ON</button></a> 
+              <a href=\"/led4off\"><button class=\"button button4\">OFF</button></a></p></div></div></div>
+              <p>C.A state: <strong> OFF</strong></p>  
+              <p><a href=\"/led5on\"><button class=\"button\">ON</button></a> 
+              <a href=\"/led5off\"><button class=\"button button5\">OFF</button></a></p></div></div></div>
+              <p>C.P state: <strong> OFF</strong></p>  
+              <p><a href=\"/led6on\"><button class=\"button\">ON</button></a> 
+              <a href=\"/led6off\"><button class=\"button button6\">OFF</button></a></p></div></div></div>
+              <p>C.C state: <strong> OFF</strong></p>  
+              <p><a href=\"/led7on\"><button class=\"button\">ON</button></a> 
+              <a href=\"/led7off\"><button class=\"button button7\">OFF</button></a></p></div></div></div>
 
-char off_resp[] = "<!DOCTYPE html><html><head><style type=\"text/css\">html {  font-family: Arial;  display: inline-block;  margin: 0px auto;  text-align: center;}h1{  color: #070812;  padding: 2vh;}.button {  display: inline-block;  background-color: #b30000; //red color  border: none;  border-radius: 4px;  color: white;  padding: 16px 40px;  text-decoration: none;  font-size: 30px;  margin: 2px;  cursor: pointer;}.button2 {  background-color: #364cf4; //blue color}.content {   padding: 50px;}.card-grid {  max-width: 800px;  margin: 0 auto;  display: grid;  grid-gap: 2rem;  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));}.card {  background-color: white;  box-shadow: 2px 2px 12px 1px rgba(140,140,140,.5);}.card-title {  font-size: 1.2rem;  font-weight: bold;  color: #034078}</style>  <title>ESP32 WEB SERVER</title>  <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">  <link rel=\"icon\" href=\"data:,\">  <link rel=\"stylesheet\" href=\"https://use.fontawesome.com/releases/v5.7.2/css/all.css\"    integrity=\"sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr\" crossorigin=\"anonymous\">  <link rel=\"stylesheet\" type=\"text/css\"></head><body>  <h2>ESP32 WEB SERVER</h2>  <div class=\"content\">    <div class=\"card-grid\">      <div class=\"card\">        <p><i class=\"fas fa-lightbulb fa-2x\" style=\"color:#c81919;\"></i>     <strong>GPIO2</strong></p>        <p>GPIO state: <strong> OFF</strong></p>        <p>          <a href=\"/led2on\"><button class=\"button\">ON</button></a>          <a href=\"/led2off\"><button class=\"button button2\">OFF</button></a>        </p>      </div>    </div>  </div></body></html>";
 
+
+      </body></html>";
+  
 static const char *TAG = "espressif"; // TAG for debug
 int led_state = 0;
 
